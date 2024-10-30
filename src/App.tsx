db577d579d2b6a36bd55b67a811758a4f92e9b02
Shi Han NG <shihanng@gmail.com>
2024-10-31 08:09:00 +0900

    Clenaup
    * Remove vite.svg and react.svg
    
    * Remove unrelated contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { GASClient } from "gas-client";
 import * as server from "./server/main";
@@ -8,7 +6,6 @@ import * as server from "./server/main";
 const { serverFunctions } = new GASClient<typeof server>();
 
 function App() {
-  const [count, setCount] = useState(0);
   const [cell, setCell] = useState("");
 
   const handleClick = async () => {
@@ -18,27 +15,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={handleClick}>{cell}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={handleClick}>Selected: {cell}</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
