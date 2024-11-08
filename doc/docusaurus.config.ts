bc6171671d261d525c14120a14a6508c63bd917d
Shi Han NG <shihanng@gmail.com>
2024-11-08 20:58:39 +0900

    doc: Add simple main page

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from "@docusaurus/preset-classic";
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Reference Check",
+  tagline: "Simple Trace Precedents and Trace Dependents in Google Sheets",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -68,7 +68,7 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "My Site",
+      title: "Reference Check",
       logo: {
         alt: "My Site Logo",
         src: "img/logo.svg",
