05bb0d6671590ce908cdec08c5fd76226181126e
Shi Han NG <shihanng@gmail.com>
2024-11-19 20:12:41 +0900

    doc: Use trailingSlash false

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -19,6 +19,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "shihanng", // Usually your GitHub org/user name.
   projectName: "reference-check", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
