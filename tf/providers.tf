608cd8983bb769b29f461815050d51a4b7bafea4
Shi Han NG <shihanng@gmail.com>
2025-04-16 22:21:21 +0900

    Split sensitive providers into providers_secret
    Prior to this change, all providers are encrypted with SOPS.
    
    This change encrypts only the one that is sensitive and
    keep the others "open".

--- tf/providers.tf
+++ tf/providers.tf
@@ -1,20 +1,24 @@
-{
-  "data": "ENC[AES256_GCM,data:zCBK+/msaEtjaBuMSq4DlH2yrCVIXjf2NQZfc9KCK8h5s2H3/WnueYprKAisJlrVLWWB3c7OlwLegs1aijB8o+sHEWKVOalHv/86p9376nJ9YQfWwvbR+sHzUq7uQynvUz2Gxe/GyNYc7ZJhitURf2xVicX0imTdHmLW93Cfx7W2P9O/rlXPbrZFgZuLWwMcqlEbKSVck/tI8rZbhjM40K19MV/nZSMcoAjIA8QyMN5KuaVaE9JUdGkdJJJCGC8XImeA0CFE+sP8jRgcw9CSFEvorHg65eDIFJOKJCSc+yh7vXwiPkOMTjE1/38gr1aTIww+wSMqdXj7ZsaeS8FWvg01pSBVehoVYwVUycI50BX4W+4TlUej/AXrO2Ub1FbdyZQNazyMP88HMLi7qGDic1QamxePjaC+aqyb1WeH9wcNbv7THjyW8vYqObBPughCW0KpzvtKlUTGxwcXa7a5pI/0BSiEAjQz1mS6GzaBSol+woXT48pLjv9psqfZOg9dJl+3azKNGYbexDC9WrpGYsMCMSBgs0tyXQXSPOhkDcuecaGIZ5+5wHUW2NL2F/s9jQ6m36YiM1Ul82XZ9J64I+DP7xXg6+7nk8Ac,iv:ROVWYy+JmjgQjdJUpPTu4/U8LogYNUlujicOJvdMcB8=,tag:BAL9UF8yOPi17ppnoK1xmw==,type:str]",
-  "sops": {
-    "kms": null,
-    "gcp_kms": null,
-    "azure_kv": null,
-    "hc_vault": null,
-    "age": [
-      {
-        "recipient": "age1jv3zzkt0kz44l5gdm0svjswd39gwg30jewqaq03vshffvavkny0sjysxdg",
-        "enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBrb0RoZXNpT1hpZktZbkR3\ncW1lZkhqQ2JoaStLZkJZY3Rjb1B1WDlBalFnCmNrV0FUOUFvcHc4TldicEJjTDNT\nLzVpby9wRDZ2SDgxeGpVd1l6ZnNNeDAKLS0tIFc2bitDVDJpandIOVBtSytHVnRl\ncStGVjh3RmNvNC9rdVN5QkhQemwzWHMKy1wf+bg/CXoSD0oYuiei/hakP3ABLQZ+\nngitCeH2jiQVTAVz2B7EpCzJpyJ/0q7rLsdACmisoJ5U1GpBSjgfJA==\n-----END AGE ENCRYPTED FILE-----\n"
-      }
-    ],
-    "lastmodified": "2025-04-16T12:43:05Z",
-    "mac": "ENC[AES256_GCM,data:apHlSBjmLztRMSXUeyxXafpaa8cCuG/5gCBo7yzPAaYlmaDpjsnJXPQjHA0ihDmgVOI5TLdBow71CIjKTXa6Ug8TapGpnhR3L+nSkZMfZXZFtPziROdTkBr6EIkUfUxMT8MruICGmbfXzWKy8iduCjV+YEKEV2hiXl2qPT8Ii80=,iv:AOEcWLYQBsIvU14mMxiJI8j4ga4EcrzMhq1TJtiYTWk=,tag:gE5nxYqFmLjRm0UV0OPs4g==,type:str]",
-    "pgp": null,
-    "unencrypted_suffix": "_unencrypted",
-    "version": "3.9.4"
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.12"
+    }
+
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
   }
+}
+
+provider "cloudflare" {}
+
+provider "github" {
+  owner = "shihanng"
 }
